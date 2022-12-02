dec529ec61eb689937b1c7d2986526f5e3ec18d1
Rolf1e <tigran.slama@contentsquare.com>
2022-12-03 00:30:43 +0100

    bit board

--- server/core/src/fr/coolnerds/minigames/boards/connectfour/ConnectFourBoard.scala
+++ server/core/src/fr/coolnerds/minigames/boards/connectfour/ConnectFourBoard.scala
@@ -1,6 +1,7 @@
 package fr.coolnerds.minigames.boards.connectfour
 
 import fr.coolnerds.minigames.boards._
+import fr.coolnerds.minigames.boards.connectfour.BitBoard.createBitBoardWithLength
 import fr.coolnerds.minigames.engine.ConnectFourConstants._
 import fr.coolnerds.minigames.engine._
 
@@ -22,6 +23,8 @@ private[connectfour] case class ConnectFourBoard(
     rowLength: Int,
     columnLength: Int
 ) extends Board[Case] {
+
+  private[connectfour] def copyCases: Seq[Case] = cases.toSeq
 
   override def at(coo: Coordinates): Option[Case] = coo match {
     case Point2D(x, y) => Some(cases((rowLength - 1) * y + x))
@@ -57,9 +60,23 @@ private[connectfour] case class ConnectFourBoard(
       .map(_._2)
   }
 
-  def isStateWon: State = RedTurn(this) // TODO
+  def isStateWon: State = {
+    implicit val bitBoard: ConnectFourBoard = this
+    (BitBoardOps.checkHasWin(yellowPon), BitBoardOps.checkHasWin(redPon)) match {
+      case (false, false) =>
+      case (true, _)              => Won(yellowPon)
+      case (_, true)              => Won(redPon)
 
-  override def isWon: Boolean = false // TODO
+    }
+  }
+
+  override def isWon: Boolean = {
+    implicit val bitBoard: ConnectFourBoard = this
+    (BitBoardOps.checkHasWin(yellowPon), BitBoardOps.checkHasWin(redPon)) match {
+      case (false, false) => false
+      case _              => true
+    }
+  }
 
   override def draw(): String = {
     var map = ""
@@ -83,4 +100,48 @@ object ConnectFourBoard {
       columnLength
     )
   }
+}
+
+private[connectfour] object BitBoardOps {
+
+  def checkHasWin(color: Case)(implicit board: ConnectFourBoard): Boolean = {
+    val bitBoard = createBitBoardWithLength(board, color)
+    var bb: Long = 0
+    for (direction <- directions(bitBoard.lastIndex)) {
+      bb = bitBoard.board & (bitBoard.board >> direction)
+      if ((bb & (bb >> (2 * direction))) != 0) {
+        return true
+      }
+    }
+    false
+  }
+
+  private def directions(width: Int): Seq[Int] = Seq(1, width, width - 1, width + 1)
+
+}
+
+case class BitBoard(board: Long, lastIndex: Int)
+
+private[connectfour] object BitBoard {
+
+  private val emptyCase = 0
+
+  // https://github.com/denkspuren/BitboardC4/blob/master/BitboardDesign.md
+  def createBitBoardWithLength(board: ConnectFourBoard, color: Case): BitBoard = {
+    var bitBoard = 0
+    val cases = board.copyCases
+
+    for ((c, i) <- cases.zipWithIndex) {
+      if (emptyCase == c) {
+        bitBoard ^= 0 << i
+      } else if (color == c) {
+        bitBoard ^= 1 << i
+      } else {
+        bitBoard ^= 0 << i
+      }
+    }
+
+    BitBoard(bitBoard, cases.length)
+  }
+
 }
